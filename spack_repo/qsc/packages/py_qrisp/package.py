c142666271015e7995729dc07f3793ad0dfe2401
Seth R Johnson <johnsonsr@ornl.gov>
2026-02-11 08:36:12 -0500

    Updates from copilot

--- spack_repo/qsc/packages/py_qrisp/package.py
+++ spack_repo/qsc/packages/py_qrisp/package.py
@@ -2,31 +2,13 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-qrisp
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-qrisp
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack_repo.builtin.build_systems.python import PythonPackage
 
 from spack.package import *
 
 
 class PyQrisp(PythonPackage):
-    """Qrisp is a high-level programming language for creating and compiling quantum algorithms.
-    """
+    """Qrisp is a high-level programming language for creating and compiling quantum algorithms."""
 
     homepage = "https://qrisp.eu"
     pypi = "qrisp/qrisp-0.7.19.tar.gz"
@@ -37,22 +19,24 @@ class PyQrisp(PythonPackage):
 
     version("0.7.19", sha256="27085566912dd101f61c8cb38f169e0da008dcbdb716586269c86ff75187ec16")
 
+
+    depends_on("py-setuptools@42:", type="build")
+    depends_on("py-wheel", type="build")
+
     depends_on("python@3.10:3.12", type=("build", "run"))
-    # depends_on("py-pip@X.Y:", type="build")
-    # depends_on("py-wheel@X.Y:", type="build")
-
-    # FIXME: Add a build backend, usually defined in pyproject.toml. If no such file
-    # exists, use setuptools.
-    # depends_on("py-setuptools", type="build")
-    # depends_on("py-hatchling", type="build")
-    # depends_on("py-flit-core", type="build")
-    # depends_on("py-poetry-core", type="build")
-
-    # FIXME: Add additional dependencies if required.
-    # depends_on("py-foo", type=("build", "run"))
-
-    def config_settings(self, spec, prefix):
-        # FIXME: Add configuration settings to be passed to the build backend
-        # FIXME: If not needed, delete this function
-        settings = {}
-        return settings
+    depends_on("py-numpy@2.0:", type=("build", "run"))
+    depends_on("py-sympy@:1.13", type=("build", "run"))
+    depends_on("py-qiskit@0.44.0:", type=("build", "run"))
+    depends_on("py-matplotlib@3.5.1:", type=("build", "run"))
+    depends_on("py-scipy@1.10.0:", type=("build", "run"))
+    depends_on("py-numba", type=("build", "run"))
+    depends_on("py-networkx", type=("build", "run"))
+    depends_on("py-tqdm", type=("build", "run"))
+    depends_on("py-dill", type=("build", "run"))
+    depends_on("py-flask@:2.3.0", type=("build", "run"))
+    depends_on("py-waitress", type=("build", "run"))
+    depends_on("py-pyyaml", type=("build", "run"))
+    depends_on("py-requests", type=("build", "run"))
+    depends_on("py-psutil", type=("build", "run"))
+    depends_on("py-jax@0.7.1", type=("build", "run"))
+    depends_on("py-jaxlib@0.7.1", type=("build", "run"))
